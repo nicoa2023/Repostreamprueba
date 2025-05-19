69651a24c49aca74ee4ec6e2defb78a94bfd37a2
nicoa2023 <128984676+nicoa2023@users.noreply.github.com>
2025-05-18 23:32:04 -0300

    Update build.gradle.kts

--- AnimeSama/build.gradle.kts
+++ AnimeSama/build.gradle.kts
@@ -5,7 +5,7 @@ version = 2
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "Anime-Sama est un site de référencement et de catalogage, créé par des passionnés de l’animation et du divertissement APAC."
+    description = "Jkanime.net est un site de référencement et de catalogage, créé par des passionnés de l’animation et du divertissement APAC."
     authors = listOf("ycngmn")
 
     /**
